24b04fc48d33e2217cbf86474e1a2eac07c2d8cc
Eric Breuers <eric@ebdigital.ca>
2020-08-12 09:35:52 -0700

    Updated run steps
    - transaction and script methods need to accept params

--- run.go
+++ run.go
@@ -6,8 +6,8 @@ import (
 
 const nonFungibleToken = "NonFungibleToken"
 const demoToken = "DemoToken"
-const rocks = "rocks"
-const auction = "auction"
+const rocks = "Rocks"
+const auction = "Auction"
 
 func main() {
 	flow := tooling.NewFlowConfigLocalhost()
@@ -17,11 +17,31 @@ func main() {
 	flow.DeployContract(rocks)
 	flow.DeployContract(auction)
 
-	flow.SendTransaction("setup/setup_account_1", demoToken)
-	flow.SendTransaction("setup/setup_account_2", rocks)
-	flow.SendTransaction("setup/setup_account_3_and_4", auction)
-	flow.SendTransaction("setup/setup_account_3_and_4", nonFungibleToken)
-	flow.SendTransaction("setup/setup_account_1_tx_minting", demoToken)
+	// Setup DemoToken account with an NFT Collection and an Auction Collection
+	flow.SendTransaction("setup/create_nft_collection", demoToken)
+	flow.SendTransaction("setup/create_auction_collection", demoToken)
 
+	// Setup Rocks account with DemoToken Vault
+	flow.SendTransaction("setup/create_demotoken_vault", rocks)
+
+	// Setup Auction Account with empty DemoToken Vault and Rock Collection
+	flow.SendTransaction("setup/create_demotoken_vault", auction)
+	flow.SendTransaction("setup/create_nft_collection", auction)
+
+	// Setup NonFungibleToken Account with empty DemoToken Vault and Rock Collection
+	flow.SendTransaction("setup/create_demotoken_vault", nonFungibleToken)
+	flow.SendTransaction("setup/create_nft_collection", nonFungibleToken)
+
+	// Mint DemoTokens for each account
+	flow.SendTransaction("setup/mint_nfts", rocks)
+	flow.SendTransaction("setup/mint_demotokens", demoToken)
+
+	// Check the balances are properly setup for the auction demo
 	flow.RunScript("check_setup")
+
+	// Add NFTs to the Auction collection for the DemoToken account
+	flow.SendTransaction("list/add_nfts_to_auction", demoToken)
+
+	// Check the auction sale data for the DemoToken account (hardcoded for now)
+	flow.RunScript("check_sales_listings")
 }
