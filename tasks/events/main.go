1ad35f366df1712425a7adb76e72c39bb5b1583e
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-08-24 20:46:12 +0200

    fixed mintint

--- tasks/events/main.go
+++ tasks/events/main.go
@@ -7,16 +7,21 @@ import (
 func main() {
 
 	// cronjob ready, read blockHeight from file
-	g := gwtf.NewGoWithTheFlow(".flow-prod.json")
+	g := gwtf.NewGoWithTheFlowMainNet()
+
+	url := "https://discord.com/api/webhooks/878741434595958824/BYFjKw9qh7SwXvWPPh8da5Yyt0alhwOyoeDEg0itGMsoD6JtW9pErQHu4YVaWdEa9vcH"
 
 	//fetch the current block height
+
 	_, err := g.EventFetcher().
 		Workers(1).
 		TrackProgressIn(".flow-prod.events").
 		EventIgnoringFields("A.d796ff17107bbff6.Versus.Bid", []string{"auctionId", "dropId"}).
 		EventIgnoringFields("A.d796ff17107bbff6.Versus.LeaderChanged", []string{"dropId"}).
+		EventIgnoringFields("A.d796ff17107bbff6.Marketplace.SaleItem", []string{"cacheKey"}).
+		EventIgnoringFields("A.d796ff17107bbff6.Marketplace.TokenPurchased", []string{"id"}).
 		Event("A.d796ff17107bbff6.Versus.Settle").
-		SendEventsToWebhook("versus-prod")
+		RunAndSendToWebhook(url)
 	if err != nil {
 		panic(err)
 	}
