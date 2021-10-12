a3c808e9766f238ba93be52650a2ee9696e0d22d
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-10-12 21:23:39 +0200

    fixed dropipfs transaction

--- tasks/dropipfs/main.go
+++ tasks/dropipfs/main.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 
 	"github.com/bjartek/go-with-the-flow/v2/gwtf"
-	"github.com/onflow/cadence"
 )
 
 func main() {
 
-	startTime := "September 24, 2021 08:00:00 AM"
+	startTime := "October 12, 2021 08:00:00 AM"
 	durationHrs := 488
 	artistAddress := "0x530212d1a4c6f7eb"
 	artist := "Sample"
-	name := "Sample4"
+	name := "Sample5"
 	editions := 0
-	content := "Qmc2LbzPHvaGzvPLuxjkp7jjVnBcxyDDXGXRdb9uhhTDme"
-	description := `This is a sample audio drop`
+	content := "QmPUwFjbapev1rrppANs17APcpj8YmgU5ThT1FzagHBxm7"
+	description := `This is a sample video drop`
 
 	//flow := gwtf.NewGoWithTheFlowMainNet()
 	flow := gwtf.NewGoWithTheFlowDevNet()
@@ -36,10 +35,9 @@ func main() {
 		UFix64Argument("4.0").                                    //min bid increment unique
 		UFix64Argument(fmt.Sprintf("%d.0", durationHrs*60*60)).   //duration 60 * 60 * 24 1 day
 		UFix64Argument("300.0").                                  //extensionOnLateBid 5 * 60 5 min
-		StringArgument("ipfs/audio").                             //type
+		StringArgument("ipfs/video").                             //type
 		UFix64Argument("0.05").                                   //artistCut 5%
 		UFix64Argument("0.025").                                  //minterCut 2.5%
-		Argument(cadence.Path{Domain: "public", Identifier: "flowTokenReceiver"}).
 		RunPrintEventsFull()
 
 }
